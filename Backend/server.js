dc7743785bf8ad78eceba9d1ae797d48869a55a5
rus.ggs <rushikeshgharkar31@gmail.com>
2026-01-04 00:55:55 +0530

    Server Running state

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get("/" , (req , res)=>{
+    res.send("Hello world")
+})
+
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
