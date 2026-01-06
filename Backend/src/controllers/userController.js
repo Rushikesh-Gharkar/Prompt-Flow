e92725afd37d3a6d03dd5d9c8b6f25e66e0fb8be
rus.ggs <rushikeshgharkar31@gmail.com>
2026-01-07 00:32:29 +0530

    User Sinup and Signin Added

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt') 
 const z = require('zod')
 const User = require('../models/User')
+const { fa } = require('zod/v4/locales')
+require('dotenv').config()
+
 
 const registerSchema = z.object({
     name : z.string().min(5).max(12),
@@ -33,6 +36,69 @@ const registerUser =async (req , res)=>{
 
     //everything is fine then hash Password
 
-    
+    const hashed_password = await bcrypt.hash(password , 10)
 
+    const user = await User.create({
+        name : name,
+        email:email,
+        password :hashed_password
+    })
+
+    return res.status(201).json({
+        success:true,
+        message:"Registration successful!"
+    })
+}
+
+const loginSchema = z.object({
+    email: z.string().email().lowercase().trim(),
+    password:z.string().min(6)
+})
+
+const loginUser = async (req , res)=>{
+    const result = await loginUser.safeParse(req.body);
+    if(!result){
+        return res.status(400).json({
+            success:false,
+            message:"Incorrect Credentials",
+            error : result.error.errors
+        })
+    }
+    
+    const {email , password} = result.data
+
+    const existing_user = await User.findOne({email})
+
+    if(!existing_user){
+        return res.status(400).json({
+            success:false,
+            message:"Incorrect email"
+        })
+    }
+
+    if(existing_user.account !== 'active'){
+        return res.status(403).json({
+            success:false,
+            message:"Active not Active"
+        })
+    }
+
+    const pass_match = await bcrypt.compare(password , existing_user.password)
+    if(!pass_match){
+        return res.status(403).json({
+            success:false,
+            message:"Invlaid credentials"
+        })
+    }
+
+    const token = await jwt.sign(
+        {userId:existing_user._id , role:existing_user.role},
+        process.env.JWT_SECR3T,
+        {expiresIn:'7d'}
+    )
+
+    return res.status(200).json({
+        success:true,
+        token:token
+    })
 }
